0c4e10cde09c489d5f3d52ef2800f30298b532b4
Patrick Jamieson <patrick@cleancodecorp.com>
2020-08-22 13:05:37 -0700

    Adds password protection to selected card page

--- src/pages/merch/topps2020.js
+++ src/pages/merch/topps2020.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
+import { MDBBtn, MDBInput } from "mdbreact"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
@@ -11,9 +12,35 @@ const ToppsProject2020Page = ({ location, data }) => {
     allStrapiTradingcard: { nodes: p2020cards }
   } = data
 
+  const [password, setPassword] = useState('')
+
+  const protectPlayerIndex = 4 // Mariano (count from most recent, starting at 0)
+  const protectPassword = "sandman"
+
+  const [playerProtected, setPlayerProtected] = useState(true)
+
   // If passed a player, open to that player. Otherwise open first player on list.
   const [value, setValue] = React.useState(location.state && location.state.player ?
     players.findIndex(s => s.name === location.state.player) : 0)
+
+    const valid = () => {
+      if (password.length > 3) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    const handleSubmit = async event => {
+      event.preventDefault()
+      event.target.className += " was-validated"
+
+      if (password === protectPassword) {
+        setPlayerProtected(false)
+      } else {
+        setPassword('')
+      }
+    }
 
   return (
     <Layout>
@@ -32,19 +59,39 @@ const ToppsProject2020Page = ({ location, data }) => {
               )
             })}
           </div>
-
+{/*console.log("protectPlayerIndex, value", protectPlayerIndex, value)*/}
           <article className="content-container">
-            <div className="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid="masonry: true">
-              {p2020cards.map((card) => {
-                return (
-                  card.project_2020_player && card.project_2020_player.name === players[value].name ?
-                   <div className="p2020" key={card.identifier}>
-                    <CardTopps2020 card={card} /></div> : null
-                )
-              })}
-            </div>
+            { (protectPlayerIndex === value && playerProtected) &&
+              <div class="card protected-card">
+                <h5 class="card-header primary-color white-text text-center py-4">
+                  <strong>Password-Protected Autograph Editions</strong>
+                </h5>
+                <div class="card-body px-lg-5 pt-0">
+                  <form class="text-center" onSubmit={(e) => handleSubmit(e)}>
+                    <div class="md-form">
+                      <MDBInput type="password" id="password" class="form-control" label="Password" value={password} required onChange={(event) => setPassword(event.target.value)}/>
+                    </div>
+                    <div className="text-center">
+                      <MDBBtn type="submit" id="submit" color="primary" disabled={!valid()}>
+                        Submit
+                      </MDBBtn>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            }
+            { (!playerProtected || protectPlayerIndex !== value) &&
+              <div className="uk-grid-small uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid="masonry: true">
+                {p2020cards.map((card) => {
+                  return (
+                    card.project_2020_player && card.project_2020_player.name === players[value].name ?
+                     <div className="p2020" key={card.identifier}>
+                      <CardTopps2020 card={card} /></div> : null
+                  )
+                })}
+              </div>
+            }
           </article>
-
         </section>
       </div>
     </Layout>
