2f8130dae808c14f32ea7d55aacef4b3f2abc21a
Patrick Jamieson <patrick@cleancodecorp.com>
2020-10-15 10:28:16 -0700

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -181,6 +181,9 @@ const Navbar = () => {
 
                 <MDBDropdownMenu>
                   <MDBDropdownItem>
+                    <Link className="menu-link" to="/topps2020/derek-jeter">Derek Jeter</Link>
+                  </MDBDropdownItem>
+                  <MDBDropdownItem>
                     <Link className="menu-link" to="/topps2020/cal-ripken-jr">Cal Ripken Jr.</Link>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
