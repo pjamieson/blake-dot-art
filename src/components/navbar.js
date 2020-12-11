555eec8f4997a33091cfb0f89e24c6abc3c3dc48
Patrick Jamieson <patrick@cleancodecorp.com>
2020-12-11 14:09:34 -0800

    Adds Frank Thomas to Topps Project 2020 cards

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -181,6 +181,9 @@ const Navbar = () => {
 
                 <MDBDropdownMenu>
                   <MDBDropdownItem>
+                    <Link className="menu-link" to="/topps2020/frank-thomas">Frank Thomas</Link>
+                  </MDBDropdownItem>
+                  <MDBDropdownItem>
                     <Link className="menu-link" to="/topps2020/tony-gwynn">Tony Gwynn</Link>
                   </MDBDropdownItem>
                   <MDBDropdownItem>
